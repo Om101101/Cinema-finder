ec365e2b64f3d5d22b553447c9cce0ee17cff585
Om Jaiswal <jom15718@gmail.com>
2026-03-21 12:34:40 +0530

    Add Home component and update dependencies for remixicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Home from './components/Home';
 
 function App() {
   return <div className="w-screen h-screen ">
