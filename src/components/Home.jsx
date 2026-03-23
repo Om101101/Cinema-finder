bcb8d7e9e26fd9a4fabde5aec5b00402da58e486
Om Jaiswal <jom15718@gmail.com>
2026-03-23 10:52:07 +0530

    Update index.html, add logo.svg, and enhance Home component layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 function Home() {
-  return (
-    <div >Home</div>
-  )
+  document.title = "SCSD| Homepage"
+  return <>
+  <div className='w-[20%] h-full bg-amber-100'></div>
+  <div className='w-[80%] h-full bg-blue-200'></div>
+  </>
 }
 
 export default Home
